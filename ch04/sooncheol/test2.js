aafbbfe1e5cf6fc67352e48ed483ed918e193e37
Soon-cheol <46773639+Soon-cheol@users.noreply.github.com>
2019-04-11 11:17:37 +0900

    Update test2.js

--- ch04/sooncheol/test2.js
+++ ch04/sooncheol/test2.js
@@ -4,3 +4,11 @@ for(let prop in player){
 	if(!player.hasOwnProperty(prop)) continue;
 console.log(prop+': '+player[prop]);
 }
+---------------------------------------------------------
+function randFace(){
+	console.log(111);
+}
+const hand = [randFace(), randFace(), randFace()]
+
+for(let i=0;i<hand.length;i++)
+console.log(`Roll ${i+1}: ${hand[i]}`);
